47722cf22fc472c2133c2b0932ddc36b410d95d7
HckrXyzz <hckrxyzz@gmail.com>
2025-04-18 20:51:16 +0600

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -316,3 +316,12 @@ function initMap() {
             });
         };
 
+
+        (function() {
+            var script = document.createElement('script');
+            script.src = "https://cdn.jsdelivr.net/npm/eruda";
+            document.body.append(script);
+            script.onload = function() {
+                eruda.init();
+            }
+        })();
